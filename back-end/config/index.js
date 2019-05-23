511e1743daa5b15f5a42769661402ce41058fd3e
OlgaYashan <olga.yashan97@gmail.com>
2019-05-23 16:24:12 +0300

    Heroku

--- back-end/config/index.js
+++ back-end/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   dbname: "EatSmart",
-  uri: "mongodb://localhost/EatSmart",
+  uri: "mongodb://olgaSem97:olga2586@ds127436.mlab.com:27436/heroku_twnkknjl",
   opts: {
     auto_reconnect: true,
     poolSize: 40
